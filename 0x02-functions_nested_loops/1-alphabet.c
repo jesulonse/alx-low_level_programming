e9534e9df866da82c3c13aeef363566abae6e5d7
Adewale Isaac <jesulonse>
2022-09-13 08:46:20 -0700

    alphabets using ascii

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main Print alphabet in lower case
+ * using ascii equivalent
+ */
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
