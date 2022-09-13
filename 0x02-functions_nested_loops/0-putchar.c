6956f7f4c46080e31f19a062c6fb1ce51ad71b24
Adewale Isaac <jesulonse>
2022-09-13 08:29:14 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  *  main - Entry point
@@ -9,7 +9,7 @@ int main(void)
 	int i;
 	char p[] = "_putchar";
 
-	for (i = 0; i < 7; i++)
+	for (i = 0; i < 8; i++)
 	{
 		_putchar(p[i]);
 	}
