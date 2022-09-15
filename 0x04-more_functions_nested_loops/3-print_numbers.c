491c81c9ff4f4bdfb8b6aa362e9af8830b2ab680
Adewale Isaac <jesulonse>
2022-09-15 06:47:53 -0700

    print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - function prints digits 0 to 9
+ *
+ * @n : input number
+ *
+ * Return: 0123456789
+ */
+void print_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
