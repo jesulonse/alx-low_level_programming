db000f02f4b94d33d70d3bb17c83714250439817
Adewale Isaac <jesulonse>
2022-09-15 07:03:18 -0700

    digits without 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function prints digits 0 to 9
+ * except 2 and 4
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c = 48; c <= 57; c++)
+	{
+		if (c = 50 || c = 52)
+		{
+			continue;
+			_putchar(c);
+		}
+	}
+	_putchar('\n');
+}
