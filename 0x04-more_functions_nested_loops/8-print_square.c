96eddfb001ce944ab00e4bb842cc6eac20da5fe7
Adewale Isaac <jesulonse>
2022-09-15 12:53:21 -0700

    sguare

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -1,4 +1,27 @@
 #include "main.h"
 
 /**
+ * print_square - print square shape of numbers
+ * n as input parameter
  *
+ * @n: input parameter
+ *
+ * Return: void
+ */
+void print_square(int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j <= i; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
