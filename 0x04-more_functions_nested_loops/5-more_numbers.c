49a99020c21c7d571f9e09aa89eb710dbd8a92ea
Adewale Isaac <jesulonse>
2022-09-15 07:39:21 -0700

    display numbers 0 to 14 ten times

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - print numbers 0 to 14
+ * 10 times
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int n;
+	char c;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (c = '0'; c <= '14'; c++)
+		{
+			if (c > 9)
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+			}
+		}
+		_putchar('\n');
+	}
+	/*_putchar('\n');*/
+}
