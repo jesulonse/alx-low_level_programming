77ed621c0487ba6552a9ecaefd2f85a9fe32b4a6
Adewale Isaac <jesulonse>
2022-09-15 10:51:31 -0700

    0 to 14 digits ten timees

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,13 @@ void more_numbers(void)
 
 	for (n = 0; n < 10; n++)
 	{
-		for (c = 0; c <= 14; c++)
+		for (c = 0; c < 15; c++)
 		{
-			if (c > 9)
+			if (c / 10 > 0)
 			{
 				_putchar((c / 10) + '0');
-				_putchar((c % 10) + '0');
 			}
+			_putchar((c % 10) + '0');
 		}
 		_putchar('\n');
 	}
