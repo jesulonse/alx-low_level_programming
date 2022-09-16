316592ab30adeff46a0f53f315eaf26e4e65d1da
Adewale Isaac <jesulonse>
2022-09-16 02:53:00 -0700

    fizzy

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,23 +11,24 @@ int main(void)
 {
 	int n;
 
-	for (n = 1; n <= 100; n++)
+	putchar('1');
+	for (n = 2; n <= 100; n++)
 	{
 		if (n % 15 == 0)
 		{
-			printf("FizzBuzz ");
+			printf(" FizzBuzz");
 		}
 		else if (n % 5 == 0)
 		{
-			printf("Buzz ");
+			printf(" Buzz");
 		}
 		else if (n % 3 == 0)
 		{
-			printf("Fizz ");
+			printf(" Fizz");
 		}
 		else
 		{
-			printf("%d ", n);
+			printf(" %d", n);
 		}
 	}
 	putchar('\n');
