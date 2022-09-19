4dfbd25409f93441eac974d501416fb3990cc640
Adewale Isaac <jesulonse>
2022-09-19 11:42:15 -0700

    print string in reverse order

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - prints string in reverse order
+ *
+ * @s: input parameter
+ * Return: NOthing
+ */
+void print_rev(char *s)
+{
+	int len;
+
+	for (len = len - 1; len >= 0; len--)
+	{
+		_putchar(s[len]);
+	}
+	_putchar('\n');
+}
+ 
