2cb6987382643af47222238172254c6f429146c6
Adewale Isaac <jesulonse>
2022-09-22 05:42:54 -0700

    leet

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string into 1337
+ * @n: input value
+ *
+ * Return: void
+*/
+char *leet(char *n)
+{
+	int i, j;
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
+
+	i = 0;
+	/*Ensuring that char is not emppty all through the iteration*/
+	for ( ; n[i] != 0; i++)
+	{
+		j = 0;
+		/*Iterating through the 10 items of the array*/
+		for ( ; j < 10; j++)
+		{
+			/*Checking if the elements of the array ae equal*/
+			if (n[i] == s1[i])
+			{
+				n[i] == s2[i];
+			}
+		}
+
+	}
+	return (n);
+}
