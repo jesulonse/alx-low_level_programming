b6cb03a8bb57bf7523e910ffa58e4d79fb4e4db6
Adewale Isaac <jesulonse>
2022-09-28 09:10:50 -0700

    string print

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+*_puts_recursion - prints a string, followed by a new line
+*
+*@s: input value
+*
+*Return: nothing
+*/
+void _puts_recursion(char *s)
+{
+	/*base case*/
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
