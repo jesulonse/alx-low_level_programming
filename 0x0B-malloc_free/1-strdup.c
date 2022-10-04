c1b47a498ef016191d64b1484fdc27e716ae4e76
Adewale Isaac <jesulonse>
2022-10-04 10:28:15 -0700

    string duplicate

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,17 +12,25 @@ char *_strdup(char *str)
 	int i, j;
 
 	if (str == NULL)
+	{
 		return (NULL);
+	}
 	i = 0;
 	while (str[i] != '\0')
+	{
 		i++;
+	}
 	i++;
 	str_dup = malloc(sizeof(*str) * i);
 	if (str_dup == NULL)
+	{
 		return (NULL);
+	}
 	j = 0;
 	while (str[j] != '\0')
+	{
 		str_dup[j] = str[j];
 		j++;
+	}
 	return (str_dup);
 }
